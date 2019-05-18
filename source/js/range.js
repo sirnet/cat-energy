bf72a90112aa4725f5c75cd11aa106dff7403d44
sirnet <artem@home-pc>
2019-05-18 22:04:28 +0300

    верстка example

--- source/js/range.js
+++ source/js/range.js
@@ -1,13 +1,14 @@
-
 var rangeImg = document.getElementById("range__b-a").max;
 var exampleWrap = document.getElementById("example__wrap").offsetWidth;
-
 var rangeInput = document.querySelector(".rangeInput");
+var before__wrap = document.querySelector(".example__before-wrap").offsetWidth;
+
 
 var example = function(width) {
   console.log(width);
   if (width >= 768){
     document.getElementById("range__b-a").max = "677";
+    document.getElementById("range__b-a").value = 677/2;
     console.log(rangeImg);
   }
 };
@@ -15,16 +16,25 @@ var example = function(width) {
 example(exampleWrap);
 
 rangeInput.oninput = function() {
+  var i = 0;
   var exampleWrap = document.getElementById("example__wrap").offsetWidth;
-  if (exampleWrap <=768)
-  if(document.getElementById("range__b-a").value == 1) {
-    document.getElementById("before").style.display = "none";
-    document.getElementById("after").style.display = "block";
+ //var before__wrap = document.querySelector(".example__before-wrap").offsetWidth;
+  i = document.getElementById("range__b-a").value;
+  console.log(i);
+
+  if(exampleWrap >=768){
+    document.querySelector(".example__before-wrap").style.width = 677 - i + "px";
+    document.querySelector(".example__after-wrap").style.width = i + "px";
   }
+
+  if (exampleWrap <=600) {
+    if(document.getElementById("range__b-a").value == 1) {
+      document.getElementById("before").style.display = "none";
+      document.getElementById("after").style.display = "block";
+    }
   if(document.getElementById("range__b-a").value == 0) {
     document.getElementById("before").style.display = "block";
     document.getElementById("after").style.display = "none";
+    }
   }
-
 }
-
